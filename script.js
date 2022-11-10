8c3261cc495201fb3d6d114341ad05fe9e5dbb1c
vinikl <108590327+vinikl@users.noreply.github.com>
2022-11-10 14:44:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+const button = document.getElementById('btn')
+const color = document.getElementById('hex')
+
+
+
+button.addEventListener('click', () => renderColor())
+
+function renderColor() {
+    let hexColor = '#'
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
